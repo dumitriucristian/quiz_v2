28d3aee3b2467c46b6ec6c2e336ecb6f9b68bcb5
dumitriucristian <dumitriucristian@yahoo.com>
2018-09-27 18:34:45 +0300

    add customSeder quiz

--- database/seeds/CustomSeeder.php
+++ database/seeds/CustomSeeder.php
@@ -11,9 +11,39 @@ class CustomSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $json = File::get("database/data/quiz.json");
+
+        $data        = json_decode($json);
+        $quiz        = $this->quiz($data);
+        $title       = $this->title($quiz);
+        $description = $this->description($quiz);
+        $questions   = $this->questions($quiz);
+
+        dd($questions);
+
+        foreach ($questions as $question) {
+            dd($question->questionText);
+        }
+
     }
 
+    private function quiz($data)
+    {
+        return $data->quiz;
+    }
 
+    private function title($quiz)
+    {
+        return $quiz->title;
+    }
 
+    private function description($quiz)
+    {
+        return $quiz->description;
+    }
+
+    private function questions($quiz)
+    {
+        return $quiz->questions;
+    }
 }
